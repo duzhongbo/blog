e36b26191b3dd9acfaeb48b45a7c4cacbfe2ed19
duzhongbo <710409599@qq.com>
2015-11-24 15:22:06 +0800

    fis配置文件修改

--- fis-conf.js
+++ fis-conf.js
@@ -6,30 +6,30 @@ fis.config.set('roadmap.path',[
         useCompile :false,
         useHash : false
     },
-	//定义html的产出路径
-	{
-		reg : '**.html',
-		release : '/debug$&'
-	},
+	// //定义html的产出路径
+	// {
+	// 	reg : '**.html',
+	// 	release : '/staticPub$&'
+	// },
 	//定义js的产出路劲
 	{
 		reg : '**.js',
-        url : '$&',
-		release : '/debug$&'
+        url : '/staticPub$&',
+		release : '/staticPub$&'
 	},
     //定义css的产出路劲
     {
         reg : '**.css',
-        url : '$&',
-        release : '/debug$&'
+        url : '/staticPub$&',
+        release : '/staticPub$&'
     },
      // 所有img目录下的.png，.gif文件
     {
         reg : /^\/img\/(.*\.(?:png|gif|jpg))/i,
         //访问这些图片的url是 '/m/xxxx?log_id=123'
-        url : '$&',
+        url : '/staticPub$&',
         //发布到/static/pic/xxx目录下
-        release : '/debug$&'
+        release : '/staticPub$&'
     },
 
 ]);
